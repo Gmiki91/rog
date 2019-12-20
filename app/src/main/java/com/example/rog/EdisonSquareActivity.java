c279a8424e98fb31795c8004b24e242d54507f78
gmiki <miklos.gabor@smartcsoport.hu>
2019-12-20 11:40:01 +0100

    first 3 activities

---
+++ app/src/main/java/com/example/rog/EdisonSquareActivity.java
@@ -0,0 +1,14 @@
+package com.example.rog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EdisonSquareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edison_square);
+    }
+}
--- app/src/main/java/com/example/rog/MainActivity.java
+++ app/src/main/java/com/example/rog/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.rog;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,MenuActivity.class));
+            }
+        },5000);
+
     }
 }
